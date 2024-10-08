f8a407fb40fe41fe4a06131bb905d53546a37023
phmuniz <phmcarvalho@hotmail.com.br>
2024-10-08 12:45:38 -0300

    configurando projeto

---
+++ src/components/Layout.tsx
@@ -0,0 +1,81 @@
+import localFont from "next/font/local"
+
+const ufes = localFont({
+    src: [
+      {
+        path: "../../public/fonts/UfesSans-Thin.ttf",
+        weight: "100",
+        style: "normal",
+      },
+      {
+        path: "../../public/fonts/UfesSans-Thin.ttf",
+        weight: "200",
+        style: "normal",
+      },
+      {
+        path: "../../public/fonts/UfesSans-Light.ttf",
+        weight: "300",
+        style: "normal",
+      },
+      {
+        path: "../../public/fonts/UfesSans-Regular.ttf",
+        weight: "400",
+        style: "normal",
+      },
+      {
+        path: "../../public/fonts/UfesSans-Medium.ttf",
+        weight: "500",
+        style: "normal",
+      },
+      {
+        path: "../../public/fonts/UfesSans-SemiBold.ttf",
+        weight: "600",
+        style: "normal",
+      },
+      {
+        path: "../../public/fonts/UfesSans-Bold.ttf",
+        weight: "700",
+        style: "normal",
+      },
+      {
+        path: "../../public/fonts/UfesSans-ExtraBold.ttf",
+        weight: "800",
+        style: "normal",
+      },
+      {
+        path: "../../public/fonts/UfesSans-ExtraBold.ttf",
+        weight: "900",
+        style: "normal",
+      },
+      {
+        path: "../../public/fonts/UfesSans-ThinItalic.ttf",
+        weight: "100",
+        style: "italic",
+      },
+      {
+        path: "../../public/fonts/UfesSans-LightItalic.ttf",
+        weight: "300",
+        style: "italic",
+      },
+      {
+        path: "../../public/fonts/UfesSans-Italic.ttf",
+        weight: "400",
+        style: "italic",
+      },
+    ],
+})
+
+interface LayoutProps {
+
+    children: any
+}
+
+export default function Layout(props: LayoutProps){
+
+    return (
+
+        <div className={ufes.className}>
+            {props.children}
+        </div>
+    )
+}
