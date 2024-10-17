f9c476b4abea3da88ef2b6b1c21194d0428b7bde
phmuniz <petengcomp.ufes@gmail.com>
2024-10-16 23:28:25 -0300

    testando api

--- src/pages/diretoria.tsx
+++ src/pages/diretoria.tsx
@@ -20,7 +20,7 @@ export default function Diretoria(){
             Eleita bienalmente e é composta por um Presidente, um VicePresidente (ambos docentes) e oito Diretores (docentes e discentes) com atribuições específicas, sendo que a composição dessa diretoria envolve dois representantes, um docente e um discente, de cada região do PET do Brasil
             </p>
 
-            <hr className="h-0.5 bg-yellow-400 w-full my-10"/>
+            <hr className='h-0.5 bg-yellow-400 w-full my-10'/>
 
             <Subtitulo valor="Membros"/>
 
