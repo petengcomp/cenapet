4eeec481bda040ac95f7ddb53b456958017e4b72
phmuniz <petengcomp.ufes@gmail.com>
2024-10-28 11:14:00 -0300

    melhorando renderizacao dos documentos

--- src/pages/documentos.tsx
+++ src/pages/documentos.tsx
@@ -8,35 +8,12 @@ export default function Documentos(){
 
     function renderizaListaDocumentos(tipo: string){
 
-        const lista = []
-        var docs = []
+        const docs = listaDocumentos.filter((doc) => doc.tipo === tipo)
 
-        for(let i = 0; i < (Math.ceil(listaDocumentos.length / 4)); i++){
+        return docs.map((documento: DocumentoModel) => (
 
-            for(let j = 0; j < 4; j++){
-
-                if(j + (4*i) >= listaDocumentos.length) break
-
-                const doc = listaDocumentos[j + (4*i)]
-
-                if(doc.tipo === tipo){
-
-                    docs.push(
-                        <Documento key={j + (4*i)} valor={doc}/>
-                    )
-                }
-            }
-
-            if(docs.length === 4){
-                lista.push(<div key={i} className="px-8 flex flex-col lg:flex-row lg:gap-40 gap-8">{docs}</div>)
-                docs = []
-            }
-        }
-
-        if(docs.length > 0)
-            lista.push(<div key={176} className="px-8 flex flex-col lg:flex-row lg:gap-40 gap-8">{docs}</div>)
-
-        return lista
+            <Documento valor={documento}/>
+        ))
     }
 
     return(
@@ -48,15 +25,15 @@ export default function Documentos(){
             <hr className="h-0.5 bg-yellow-400 w-full my-10"/>
             
             <Subtitulo valor="Portaria"/>
-            <div className="flex flex-col justify-center gap-8 lg:gap-16">{renderizaListaDocumentos('portaria')}</div>
+            <div className="flex flex-wrap justify-between w-[90%] mx-auto">{renderizaListaDocumentos('portaria')}</div>
             <hr className="h-0.5 bg-yellow-400 w-full my-10"/>
 
             <Subtitulo valor="Legislação"/>
-            <div className="flex flex-col justify-center gap-8 lg:gap-16">{renderizaListaDocumentos('legislacao')}</div>
+            <div className="flex flex-wrap justify-between w-[90%] mx-auto">{renderizaListaDocumentos('legislacao')}</div>
             <hr className="h-0.5 bg-yellow-400 w-full my-10"/>
 
             <Subtitulo valor="Custeio"/>
-            <div className="flex flex-col justify-center gap-8 lg:gap-16">{renderizaListaDocumentos('custeio')}</div>
+            <div className="flex flex-wrap justify-between w-[90%] mx-auto">{renderizaListaDocumentos('custeio')}</div>
         </Layout>
     )
 }
